774e00ab174f7e63f14b550541d23afdfd687993
SuzaanAhmed <SuzaanAhmed00@gmail.com>
2025-01-14 15:17:11 +0530

    fethcing snippet used

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,8 +2,19 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 import { Height } from '@mui/icons-material';
-import Sidebar from './Sidebar';
+import {Sidebar,Videos} from './index';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 const Feed = () => {
+
+  const [selectedCategory,setselectedCategory]=useState('New');
+  
+  const [videos,setVideos]=useState([])
+
+  useEffect=(()=>{
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+    .then((data)=>setVideos(data.items))
+  },[selectedCategory])
+
   return (
     <Stack sx={{ 
       flexDirection: { sx: "column", md: "row" } 
@@ -13,10 +24,20 @@ const Feed = () => {
           borderRight: '1px solid #3d3d3d', 
           px: { sx: 0, md: 2 } 
         }}>
-        <Sidebar/>
+        <Sidebar 
+        selectedCategory={selectedCategory}
+        setselectedCategory={setselectedCategory}/>
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: '#fff' }}>
           Copyright 2025 React
         </Typography>
+      </Box>
+
+      <Box p={2} sx={{overflowY: 'auto', flex: 2, height: "90vh" }}>
+        <Typography variant='h4' fontWeight="bold" mb={2} 
+                    sx={{color: 'white'}}>
+          {selectedCategory} <span style={{color: '#F31503'}}> videos</span>
+        </Typography>
+        <Videos videos={videos}/>
       </Box>
     </Stack>
   )
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Stack} from '@mui/material';
 import { categories } from '../utils/constants';
 
 
-const Sidebar = () => {
+const Sidebar = ({selectedCategory,setselectedCategory}) => {
+
+  
   return (
     <Stack
       direction="row"
       sx={{
-        overflowY: "auto",
+        overflow: "auto",
         height: { sx:'auto', md:'95%'},
         flexDirection: {md: 'column'},
       }}
     >
       {categories.map((category) => (
-        <button>
-          <span>{category.icon}</span>
-          <span>{category.name}</span>        
+        <button
+          className="category-btn"          style={{
+          background: category.name===selectedCategory && '#FC1503',
+          color: "white"
+          }}
+          key={category.name}
+          onClick={()=>setselectedCategory(category.name)}
+        >
+          <span style={{color: category.name===selectedCategory ? 'white' : 'red',
+                        marginRight: '15px'
+          }}>{category.icon}</span>
+
+          <span style={{opacity: category.name===selectedCategory ? '1':'0.8'}}>{category.name}</span>       
         </button>
+        
       ))}
 
     </Stack>
